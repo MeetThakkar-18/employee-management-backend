6167a539a508b88abfb1d451deaa9db7e9d0b480
Meet Thakkar <meetthakkar8336@gmail.com>
2024-03-23 10:33:31 +0530

    fix: resolved server.js location

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
-const employeeRoutes = require('./routes/employeeRoutes');
+const employeeRoutes = require('./src/routes/employeeRoutes');
 
 const app = express();
 
